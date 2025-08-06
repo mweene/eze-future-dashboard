30f4da62d78ed8353cb83a2bc6c4ce2e3a28f96e
mweene <harrisonmweene11@gmail.com>
2025-08-06 22:59:49 +0200

    Start EditClientModal Form from scratch

--- src/components/EditClientModal.jsx
+++ src/components/EditClientModal.jsx
@@ -1,39 +1,21 @@
 import { useState } from "react";
-import AddNewClientModal from "./AddNewClientModal";
-import InputWithLabel from "./InputWithLabel";
 
 const EditClientModal = ({ client }) => {
-  const [formData, setFormData] = useState(client);
+  const [formData, setFormData] = useState({
+    id: client.id,
+    name: client.name,
+  });
+
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: { value } }));
+    setFormData((prev) => ({ ...prev, id: e.target.value }));
+    console.log(formData.id);
   };
-  console.log("component has reloaded");
   return (
-    <div
-      key={client.id}
-      className="absolute top-0 right-0 left-0 m-10 p-4 z-20 w-3xs bg-white border border-gray-300"
-    >
-      <form className="">
-        {Object.entries(formData).map(([key, value]) => {
-          if (value !== null && typeof value === "object")
-            return Object.keys(value).map((k) => (
-              <InputWithLabel
-                key={k}
-                label={k}
-                value={formData[key][k]}
-                onChange={handleChange}
-              />
-            ));
-          return (
-            <InputWithLabel
-              key={key}
-              label={key}
-              value={formData[key]}
-              onChange={handleChange}
-            />
-          );
-        })}
+    <div className="absolute p-8 bg-blue-100 top-0 left-0 z-20">
+      <form action="" className="bg-white">
+        <label htmlFor="id">
+          <input type="text" value={formData.id} onChange={handleChange} />
+        </label>
       </form>
     </div>
   );
